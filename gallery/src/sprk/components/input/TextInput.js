d584a95696508e36392a9aafa4484e368eab8d63
Daniel Sims <danielsims@quickenloans.com>
2018-02-04 23:29:42 -0500

    Adding in monetary and some other classes

--- gallery/src/sprk/components/input/TextInput.js
+++ gallery/src/sprk/components/input/TextInput.js
@@ -1,20 +1,32 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import classNames from 'classnames';
 
+//@todo - for Monetary, blur to format value
 class TextInput extends React.Component {
+    constructor(props) {
+        super(props);
+        this.blur = this.blur.bind(this);
+    }
+
     render() {
         const { label, error, placeholder, type, id, helperText, disabled} = this.props;
+        const classes = classNames('sprk-b-TextInput', 'sprk-u-Size-100', {
+            sprk-b-TextInput--error: error === true,
+            sprk-b-TextInput--monetary: type === 'monetary',
+        });
+
         return (<React.Fragment>
             <label htmlFor={id}>{label}</label>
-            <input disabled={disabled} className="sprk-b-TextInput sprk-u-Size-100" id={id} type={type} placeholder={placeholder} />
+            <input disabled={disabled} className={classes} id={id} type={type} placeholder={placeholder} />
             {helperText && <span className="sprk-b-HelperText">{helperText}</span>}
         </React.Fragment>);
     }
 }
 
 TextInput.propTypes = {
-    type: PropTypes.oneOf(['text', 'email', 'password']),
-    error: PropTypes.string,
+    type: PropTypes.oneOf(['text', 'email', 'password', 'monetary']),
+    error: PropTypes.bool,
     id: PropTypes.string,
     helperText: PropTypes.string,
     disabled: PropTypes.bool
